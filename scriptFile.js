262d7876bb34582c71d32b7e32758e93ef3024b8
raja67m <rajalingam210901@gmail.com>
2025-11-19 18:41:12 +0530

    search function added

--- scriptFile.js
+++ scriptFile.js
@@ -21,7 +21,25 @@ bookAppointment=()=>{
 } 
 
 
+// search function
+function searchBar(){
+   let input=document.getElementById('search').value;
+    input=input.toLowerCase();
+    let x=document.querySelectorAll(".feature-content");
+ 
+    for(i=0;i<x.length;i++){
+      if(input === ''){
+         x[i].style.display='';
 
+      }else if(!x[i].innerHTML.toLowerCase().includes(input)){
+         x[i].style.display="none";
+        
+      }else{
+         x[i].style.display='feature-content';
+      
+      }
+    }
+}
 
 
 
